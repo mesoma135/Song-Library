6d67dd0ab5bf1e36623795171b06ea5da2028a3a
mesoma135 <146199540+mesoma135@users.noreply.github.com>
2025-11-17 06:34:31 -0500

    added data validation in relevant fields (i.e login)

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const { registerValidation, loginValidation } = require("../validators/authValidators");
+const validate = require("../middleware/validatorMiddleware");
 
 //new User Registration
-router.post('/register', authController.register);
+router.post('/register', registerValidation, validate, authController.register);
 
 //User Login
-router.post('/login', authController.login);
+router.post('/login', loginValidation, validate, authController.login);
 
 //User Logout with user authentication
 router.post('/logout', authController.logout);
--- Backend/routes/playlistRoutes.js
+++ Backend/routes/playlistRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const playlistController = require('../controllers/playlistController');
 const authMiddleware = require('../middleware/authMiddleware');
+const { createPlaylistValidation } = require('../validators/playlistValidator');
+const validate = require('../middleware/validatorMiddleware');
 
 // CREATE playlist (requires token)
-router.post('/', authMiddleware, playlistController.createPlaylist);
+router.post('/', authMiddleware, createPlaylistValidation, validate, playlistController.createPlaylist);
 
 // GET playlist by ID (requires token)
 router.get('/:id', authMiddleware, playlistController.getPlaylist);
